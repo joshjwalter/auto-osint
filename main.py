1d28e87f5696cd4ef1a63f046c97218fad971a1d
jjwalter368 <jjw368@gmail.com>
2024-12-19 00:12:19 -0500

    added a profile.py module for building a profile based on information, includes class Profile

--- main.py
+++ main.py
@@ -2,7 +2,34 @@ import requests
 import json
 import cryptography
 
+
+
+
 def login():
-    username = input("Username: ")
     password = input("Password: ")
-    # check username with login list json, hash password to check if it matches
+    if password == "7933":
+        main()
+    else:
+        print("Password incorrect, try again")
+        login()
+
+def main():
+    print("""
+  ___        _              _____ _____ _____ _   _ _____ 
+ / _ \      | |            |  _  /  ___|_   _| \ | |_   _|
+/ /_\ \_   _| |_ ___ ______| | | \ `--.  | | |  \| | | |  
+|  _  | | | | __/ _ \______| | | |`--. \ | | | . ` | | |  
+| | | | |_| | || (_) |     \ \_/ /\__/ /_| |_| |\  | | |  
+\_| |_/\__,_|\__\___/       \___/\____/ \___/\_| \_/ \_/  
+     By Joshua Walter                                                     
+                                                          
+    """)    
+    mode = input("Which mode would you like to use:\n1) New Report\n2) Continue Report\n 3) View Finished Reports\n 4) Custom Search")
+    match mode:
+        case "1":
+            # ask for email phone number etc.
+            # consider making a class/object
+            # profile = buildProfile()
+            # use the info from build profile to make report
+            # report() maybe add this function to a seperate file
+
---
+++ profile.py
@@ -0,0 +1,15 @@
+class Profile:
+    def __init__(self, firstName, lastName, phoneNumber, email, username, address, zipCode, domain, ip):
+        #Make sure to add that if a value is set to None, don't try to send it through the api's
+        self.name = (firstName, lastName)
+        self.phoneNumber = phoneNumber
+        self.email = email
+        self.username = username
+        #Might need to change address into a tuple or array with street, zip code, city, state, etc.
+        self.address = address
+        #Might be depricated for address tuple/array
+        self.zipCode = zipCode
+        self.domain = domain
+        self.ip = ip
+        #add a variable for the report, probably a dictionary that can be converted to json for saving
+    #add function to save to db, do a report, display report
