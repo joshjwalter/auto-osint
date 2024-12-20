5b481ca45e270260f154221b30f09c4fd2f7e2d8
jjwalter368 <jjw368@gmail.com>
2024-12-20 10:31:44 -0500

    adding poetry package managment and some comments

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ def main():
                                                           
     """)    
     mode = input("Which mode would you like to use:\n1) New Report\n2) Continue Report\n 3) View Finished Reports\n 4) Custom Search")
-    match mode:
-        case "1":
+#    match mode:
+#        case "1":
             # ask for email phone number etc.
             # consider making a class/object
             # profile = buildProfile()
--- profile.py
+++ profile.py
@@ -1,4 +1,8 @@
+#Use to display the Report like sherlock does
+import termcolor
+
 class Profile:
+    # check username with login list json, hash password to check if it matches
     def __init__(self, firstName, lastName, phoneNumber, email, username, address, zipCode, domain, ip):
         #Make sure to add that if a value is set to None, don't try to send it through the api's
         self.name = (firstName, lastName)
@@ -11,5 +15,7 @@ class Profile:
         self.zipCode = zipCode
         self.domain = domain
         self.ip = ip
-        #add a variable for the report, probably a dictionary that can be converted to json for saving
+    class Report:
+        def __init__(self, firstName, lastName, phoneNumber, email, username, address, zipCode, domain, ip):
+
     #add function to save to db, do a report, display report
